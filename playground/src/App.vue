7b969bd08ff66002f2039762eebbf7af4ebbf89a
lanyue <lanyueos@qq.com>
2022-04-17 23:50:47 +0800

    fix: 更新脚手架到vite(WIP)

---
+++ playground/src/App.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+import { reactive, ref, h } from 'vue'
+</script>
+
+<template>
+  <div>Hello</div>
+</template>
+
+<style lang="less">
+body {
+  font-family: -apple-system,BlinkMacSystemFont,Helvetica Neue,PingFang SC,Microsoft YaHei,Source Han Sans SC,Noto Sans CJK SC,WenQuanYi Micro Hei,sans-serif;
+  font-size: 15px;
+  color: #121212;
+}
+</style>
